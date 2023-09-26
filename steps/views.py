8a8b9f1f3577e19e1ea69ff69dd226fe3f8a3216
Nikola Bozhinov <nikolaobozhinov@gmail.com>
2023-09-26 11:05:25 +0300

    add functions

--- steps/views.py
+++ steps/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+
+def dashboard(request):
+    return render(request, "steps/index.html")
+
+
+def map(request):
+    pass
+    # return render(request, "steps/map.html")
+
+
+def clustering(request):
+    pass
+    # return render(request, "steps/clustering.html")
+
+
+def data_view(request):
+    pass
+    # return render(request, "steps/data_view.html")
+
+
+def steps_settings(request):
+    pass
+    # return render(request, "steps/steps_settings.html")
