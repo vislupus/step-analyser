304ca89ffdc78707e5688a767462c39763e65652
Nikola Bozhinov <nikolaobozhinov@gmail.com>
2023-10-10 14:35:02 +0300

    add new functions

--- steps/views.py
+++ steps/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.http import Http404, HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import (
+    Http404,
+    HttpResponse,
+    HttpResponseNotFound,
+    HttpResponseRedirect,
+)
 from django.urls import reverse
 
 
@@ -7,20 +12,29 @@ def dashboard(request):
     return render(request, "steps/dashboard.html")
 
 
-def map(request):
-    return render(request, "steps/map.html")
+def data_view(request):
+    return render(request, "steps/data_view.html")
+
+
+def calendar(request):
+    return render(request, "steps/calendar.html")
 
 
 def clustering(request):
     return render(request, "steps/clustering.html")
 
 
-def data_view(request):
-    return render(request, "steps/data_view.html")
-
 def trips(request):
-    return HttpResponse("Trips")
+    return render(request, "steps/trips.html")
+
+
+def tourist_attractions(request):
+    return render(request, "steps/tourist_attractions.html")
+
+
+def visited_countries(request):
+    return render(request, "steps/visited_countries.html")
 
 
 def steps_settings(request):
-    return render(request, "steps/steps_settings.html")
+    return render(request, "steps/settings.html")
